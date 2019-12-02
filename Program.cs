fe9a233e733af53af069cb0b1b5700a894f4457c
altayevrim <evrimaltay@gmail.com>
2019-12-02 19:05:04 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Matrix New Project
+Just another boring project for ONPU.
+Enjoy and try to not sleep because of the boredom while using it.
+
+Evrim Altay KOLUAÇIK
+info@rimtay.com
+Odesa - Dec 02 19
